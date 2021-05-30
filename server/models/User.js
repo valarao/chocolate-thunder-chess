345aceee7ff89a6e2ddb7216897025e51083477b
unknown <miguel.valarao@gmail.com>
2021-05-30 15:02:20 -0700

    Setup database connection with server

---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// TODO: This is a placeholder. Update after database schema design is complete. 
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('User', userSchema, 'users');
