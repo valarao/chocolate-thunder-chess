573c84ebd0c7b9eee4505db2c2cbf8876683eadf
Miguel Valarao <miguel.valarao@gmail.com>
2021-06-01 02:02:55 -0700

    Implement Position Card Dashboard and Image Generation Script (#30)
    * Add image generator script
    
    * Implement position card components and image generation script
    
    * Clean up code styling
    
    * Clean up code styling
    
    * Change minimum width
    
    * Remove extra space
    
    * Improve responsiveness with breakpoints

--- client/src/components/dashboard/PositionCardContainer.js
+++ client/src/components/dashboard/PositionCardContainer.js
@@ -2,32 +2,48 @@ import React from 'react';
 
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 
-const useStyles = makeStyles({
+import PositionCard from './PositionCard';
+import { getMockPositions } from '../../mock/positions';
+
+const useStyles = makeStyles(theme => ({
   root: {
-    textAlign: 'center',
+    display: 'flex',
   },
   paper: {
-    backgroundColor: 'white',
-    margin: '0% 10%',
-    minHeight: '600px',
+    backgroundColor: '#d3d3d3',
+    margin: '0 auto',
+    padding: '0 0.5rem',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    marginBottom: '2rem',
+    width: '60%',
+    [theme.breakpoints.between('sm', 'md')]: {
+      width: '75%',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '95%',
+    },
   },
-  text: {
-    textAlign: 'center',
+  cardWrapper: {
+    display: 'inline-block',
+    margin: '0.5rem 0.5rem',
   },
-});
+}));
 
 const PositionCardContainer = () => {
   const classes = useStyles();
-
+  // TODO: Replace mock data with axios call
+  const positions = getMockPositions();
   return (
     <Box className={classes.root}>
       <Paper className={classes.paper}>
-      <Typography className={classes.text}>
-        Insert Position Card Container
-      </Typography>
+        {positions.map((position) => (
+          <Box className={classes.cardWrapper} key={position.name}>
+            <PositionCard position={position} />
+          </Box>
+        ))}
       </Paper>
     </Box>
   );
---
+++ docs/scripts/001-ImageGenerator.md
@@ -0,0 +1,14 @@
+# Image Generator
+
+## Overview
+This Image Generator script takes in a folder of `.pgn` files and generates `.png` images as well as `.b64` encoding.
+
+This can be used for creating images for mock data on the front end.
+
+## Instructions
+1. Use command line to `cd` into the `image-generator` folder
+2. Add `.pgn` files with positions into the `/pgn` folder
+3. Run `node imageGenerator.js`
+
+## Future Work
+In the future, we will convert this script into a backend endpoint.
