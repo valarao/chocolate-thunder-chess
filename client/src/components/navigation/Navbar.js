aeafefc039d9e73be966c858b405107dd29a5d32
AlvinCYLo <alvin.cy.lo@gmail.com>
2021-06-06 20:33:19 -0700

    Added centered navbar

--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Box from "@material-ui/core/Box";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
 
 const useStyles = makeStyles({
   root: {
-    backgroundColor: 'white',
-    padding: '2rem',
-    marginBottom: '2rem',
+    backgroundColor: "white",
+    padding: "2rem",
+    marginBottom: "2rem",
   },
   text: {
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
 
+// TODO: Add functionality for page changes when react-router is implemented
 const Navbar = () => {
   const classes = useStyles();
+  const [value, setValue] = React.useState("Home");
+
+  const handleChange = (event, value) => {
+    setValue(value);
+  };
 
   return (
     <Box className={classes.root}>
-      <Typography className={classes.text}>
-        Insert Navbar
-      </Typography>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab value="Home" label="Home" />
+        <Tab value="About" label="About" />
+        <Tab value="Info" label="Opening Info" />
+        <Tab value="Custom Notations" label="Custom Notiations" />
+      </Tabs>
     </Box>
   );
 };
