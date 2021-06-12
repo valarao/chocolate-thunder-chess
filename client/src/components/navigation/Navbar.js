c0e6a0ac7d2a8090e1c774b8dd62dc33b5b9d890
AlvinCYLo <alvin.cy.lo@gmail.com>
2021-06-11 20:21:33 -0700

    Single quote

--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 
-import makeStyles from "@material-ui/core/styles/makeStyles";
-import Box from "@material-ui/core/Box";
-import Tab from "@material-ui/core/Tab";
-import Tabs from "@material-ui/core/Tabs";
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import Box from '@material-ui/core/Box';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
 
 const useStyles = makeStyles({
   root: {
-    backgroundColor: "white",
-    padding: "1rem ",
-    marginBottom: "2rem",
+    backgroundColor: 'white',
+    padding: '1rem ',
+    marginBottom: '2rem',
   },
   text: {
-    textAlign: "center",
+    textAlign: 'center',
   },
 });
 
 // TODO: Add functionality for page changes when react-router is implemented
 const Navbar = () => {
   const classes = useStyles();
-  const [value, setValue] = React.useState("Home");
+  const [value, setValue] = React.useState('Home');
 
   const handleChange = (event, value) => {
     setValue(value);
@@ -30,15 +30,15 @@ const Navbar = () => {
       <Tabs
         value={value}
         onChange={handleChange}
-        indicatorColor="primary"
-        textColor="primary"
-        scrollButtons="auto"
-        variant="fullWidth"
+        indicatorColor='primary'
+        textColor='primary'
+        scrollButtons='auto'
+        variant='fullWidth'
       >
-        <Tab value="Home" label="Home" />
-        <Tab value="About" label="About" />
-        <Tab value="Info" label="Opening Info" />
-        <Tab value="Custom Notations" label="Custom Notiations" />
+        <Tab value='Home' label='Home' />
+        <Tab value='About' label='About' />
+        <Tab value='Info' label='Opening Info' />
+        <Tab value='Custom Notations' label='Custom Notiations' />
       </Tabs>
     </Box>
   );
