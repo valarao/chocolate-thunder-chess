ba4006b803b55da43d20f1d8a1d39210d7d61b64
AlvinCYLo <alvin.cy.lo@gmail.com>
2021-06-12 17:05:34 -0700

    Linting

--- client/src/redux/reducers/notationReducer.js
+++ client/src/redux/reducers/notationReducer.js
@@ -1,20 +1,19 @@
 import { GET_NOTATION_POSITION } from '../types';
 
 const initialState = {
-    notation: null,
-}
+  notation: null,
+};
 
 const notationReducer = (state = initialState, action) => {
-    switch (action.type) {        
-        case GET_NOTATION_POSITION:
-            return {
-                ...state,
-                notation: action.payload,
-            };
-        default:
-            return state;
-    }
-}
-
+  switch (action.type) {
+    case GET_NOTATION_POSITION:
+      return {
+        ...state,
+        notation: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default notationReducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,14 +4,16 @@ import thunk from 'redux-thunk';
 
 const initialState = {};
 const rootReducer = combineReducers({
-    notations: notationReducer,
+  notations: notationReducer,
 });
 
 const store = createStore(
-    rootReducer,
-    initialState, 
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
