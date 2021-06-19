0997fa88417855a5c77f44130dcfae7499093c36
unknown <miguel.valarao@gmail.com>
2021-06-18 21:20:25 -0700

    Improve endpoint naming

--- server/tests/positions.spec.js
+++ server/tests/positions.spec.js
@@ -21,10 +21,10 @@ describe('Positions', function () {
     });
   });
 
-  describe(`GET ${positionsBaseRoute}/common-positions`, function () {
+  describe(`GET ${positionsBaseRoute}/common`, function () {
     it('should return an array of positions', function (done) {
       chai.request(app)
-        .get(`${baseRoutes.positions}/common-positions`)
+        .get(`${baseRoutes.positions}/common`)
         .end((_err, res) => {
           res.should.have.status(200);
           res.body.should.have.property('positions').and.to.be.an('array').and.to.have.property('length', 9);
