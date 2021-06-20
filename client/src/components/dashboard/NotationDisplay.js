61c395c29eba0c5948d8dbac73ca240951693a8b
unknown <miguel.valarao@gmail.com>
2021-06-20 12:37:16 -0700

    Add link

--- client/src/components/dashboard/NotationDisplay.js
+++ client/src/components/dashboard/NotationDisplay.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Tooltip from '@material-ui/core/Tooltip';
+
+import CloseIcon from '@material-ui/icons/Close';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 
 const useStyles = makeStyles(theme => ({
   dialog: {
@@ -112,7 +115,7 @@ const NotationDisplay = (props) => {
             </Tooltip>
           </Box>
           <Box>
-            <Button className={classes.buttons} variant='outlined'>Play</Button>
+            <Button className={classes.buttons} component={Link} to='/game' variant='outlined'>Play</Button>
             <Button className={classes.buttons} variant='outlined' onClick={handleClose}>Cancel</Button>
           </Box>
         </DialogContent>
